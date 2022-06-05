055917b0e0733b9e59d9529d75cbce90b7641c7a
Lucas Kaua <lucasdcunha@gmail.com>
2022-06-05 18:42:39 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/tdd/service/CalculadoraTeste.java
@@ -0,0 +1,12 @@
+package br.com.alura.tdd.service;
+
+import br.com.alura.tdd.modelo.Calculadora;
+
+public class CalculadoraTeste {
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+        int soma = calc.somar(7,3);
+        
+
+    }
+}
